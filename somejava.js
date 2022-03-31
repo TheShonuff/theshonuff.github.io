1800f484b2059b865eff6dc0880be6a22e544a02
TheShonuff <jirvine124@gmail.com>
2022-03-30 23:03:03 -0700

    add animations and fix styles

--- somejava.js
+++ somejava.js
@@ -1,4 +1,41 @@
-function toggle(){
+function toggle() {
   $("#checkbox_toggle").prop("checked", false);
 }
 
+const scrollElements = document.querySelectorAll(".js-scroll");
+
+// scrollElements.forEach((el) => {
+//   el.style.opacity = 0;
+// });
+
+const elementInView = (el, percentageScroll = 100) => {
+  const elementTop = el.getBoundingClientRect().top;
+
+  return (
+    elementTop <=
+    (window.innerHeight || document.documentElement.clientHeight) *
+      (percentageScroll / 100)
+  );
+};
+
+const displayScrollElement = (element) => {
+  element.classList.add("scrolled");
+};
+
+const hideScrollElement = (element) => {
+  element.classList.remove("scrolled");
+};
+
+const handleScrollAnimation = () => {
+  scrollElements.forEach((el) => {
+    if (elementInView(el, 70)) {
+      displayScrollElement(el);
+    } else {
+      hideScrollElement(el);
+    }
+  });
+};
+
+window.addEventListener("scroll", () => {
+  handleScrollAnimation();
+});
